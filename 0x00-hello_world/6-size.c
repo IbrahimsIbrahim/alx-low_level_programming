754d2d8346e3438b37b72959a776b409f6370387
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-21 12:42:31 -0700

    preprocessor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* You should produce the exact same output as in the example
+* Warning are allowed 
+* 
+* You program should return 0
+*/
+int main(void)
+{
+printf("size for a char : %d \n", sizeof(char));
+printf("size for an int : %d \n", sizeof(int));
+printf("size for a long int : %d \n", sizeof(long int));
+printf("size for a long long int : %d \n", sizeof(long long int));
+printf("size for a float : %d \n", sizeof(float));
+return (0);
+}
