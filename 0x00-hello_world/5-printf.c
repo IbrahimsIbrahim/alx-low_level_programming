b107bdb0c8282c9f399a5581775a1b5632381290
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-21 12:25:23 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 * main - print the string in the main function
 *
 * Description: using the main function
-* this process prints "with proper grammar, but the outcome is a piece of art,"
-* Return 0
+* this process prints with proper grammar, but the outcome is a piece of art,
+* Return: 0
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art, \n");
-return 0;
-}  
+return (0);
+}
