18d893ce1599ce2d24ba124f4e4317ad3f007725
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-21 14:13:08 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 /**
 * You should produce the exact same output as in the example
 * Warnings are allowed
-* 
 * Your program should return 0
 */
 int main(void)
@@ -12,5 +11,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
-return(0);
+return (0);
 }
