5912b58bc709090fe3484cad9e2599800b45096d
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-28 10:12:12 -0700

    task complete

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,8 @@
  * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
- int main()
+ int main(void)
  {
     printf("_putchar \n");
-    return 0;
+    return (0);
  }
