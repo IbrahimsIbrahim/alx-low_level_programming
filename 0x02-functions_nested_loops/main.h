35a44bf169f912855321c093b0823c9bd686b5c1
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-29 04:51:01 -0700

    task complete

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -28,7 +28,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
