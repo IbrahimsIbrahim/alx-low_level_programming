4332f4dcb36667cca34e32bdafaa6b490b6d04bf
IbrahimsIbrahim <isibraheem94@gmail.com>
2023-05-29 05:45:24 -0700

    task complete

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,23 +10,22 @@
  */
 int print_sign(int n)
 {
-	int value;
+        int value;
 
-	if (n > 0)
-	{
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
-	else
-	{
-		value = -1;
-		_putchar('-');
-	}
-
-	return (value);
+        if (n > 0)
+        {
+                value = 1;
+                _putchar('+');
+        }
+        else if (n == 0)
+        {
+                value = 0;
+                _putchar('0');
+        }
+        else
+        {
+                value = -1;
+                _putchar('-');
+        }
+        return (value);
 }
